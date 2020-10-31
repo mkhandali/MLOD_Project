c02087aa41a23655db7babe7340ef2cd7dd5acf5
khandali <khandali@khandali-A0000001>
2020-10-31 21:43:56 +0100

    Eval

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "mpc.h" // parser lib
 
 #include <editline/readline.h>
@@ -12,22 +13,56 @@
 
 
 
+long eval_op(long x, char* op, long y) {
+  if (!strcmp(op, "+")) return x + y;
+  if (!strcmp(op, "-")) return x - y;
+  if (!strcmp(op, "*")) return x * y;
+  if (!strcmp(op, "/")) return x / y;
+  if (!strcmp(op, "%")) return x % y;
+  if (!strcmp(op, "^")) return pow(x,y);
+  if (!strcmp(op, "min")) return (x<y ? x : y);
+  if (!strcmp(op, "max")) return (x<y ? y : x);
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  /* If tagged as number return it directly. */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* The operator is always second child. */
+  char* op = t->children[1]->contents;
+
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining. */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
+
 void parsing(char* line,mpc_parser_t* Zezay){
 
-    /* Attempt to parse the user input */
     mpc_result_t r;
+
     if (mpc_parse("<stdin>", line, Zezay, &r)) {
-      /* On success print and delete the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
+
     } else {
-      /* Otherwise print and delete the Error */
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
 }
-
-
 
 int main(int argc, char** argv) {
 
@@ -41,7 +76,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                     \
       number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' ;                  \
+      operator : '+' | '-' | '*' | '/' | '%' | '^' | \"max\" | \"min\" ; \
       expr     : <number> | '(' <operator> <expr>+ ')' ;  \
       lispy    : /^/ <operator> <expr>+ /$/ ;             \
     ",
